5cb3078a9a40a537f8c5b646cc3ff972d4654f26
quy-tran-dev <quyto113@gmail.com>
2025-08-30 18:21:01 +0700

    create module simulator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SimulatorModule } from './simulator/simulator.module';
 
 @Module({
-  imports: [],
+  imports: [SimulatorModule],
   controllers: [AppController],
   providers: [AppService],
 })
